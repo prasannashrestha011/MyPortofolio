1b0fd08908ed2c00aa8219fdf952499c00066e08
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-10-11 09:35:54 +0545

    font change#1

--- client-side/app/ViewComponents/MainView.tsx
+++ client-side/app/ViewComponents/MainView.tsx
@@ -11,10 +11,10 @@ const MainView = () => {
       flex  justify-between  
       h-96 mx-auto w-8/12 md:mt-10 lg:mt-10'
       >
-      <span className='text-lg md:text-4xl  justify-start items-center   '>
+      <span className='text-4xl  justify-start items-center   '>
         <p className='font-semibold'>Prasanna Shrestha</p>
         <br/>
-        <p className='md:text-2xl text-md '>fullstack developer, build Web & Desktop apps</p>
+        <p className='text-2xl  '>fullstack developer, build Web & Desktop apps</p>
         </span>
       
       </div>
