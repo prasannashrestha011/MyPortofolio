8a2d8612f4915ca714d1d3d4f71b4e44dd65a576
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-10-12 21:57:13 +0545

    nav change#1

--- client-side/app/ViewComponents/NavBar.tsx
+++ client-side/app/ViewComponents/NavBar.tsx
@@ -6,11 +6,11 @@ import {motion} from 'framer-motion'
 const NavBarLinks=()=>{
  return(
   <>
-   <ul className='flex flex-col '>
+   <ul className='flex flex-col gap-2 '>
    {NavItems.map((item,idx)=>{
     return(
      
-           <li key={idx} className=' md:w-28 w-14 md:h-14 h-12  flex justify-center items-center'>
+           <li key={idx} className=' md:w-28 w-14 md:h-14 h-12  flex justify-center items-center md:text-2xl text-xl'>
       <a href={item.link}>{item.name}</a>
       </li>
   
@@ -48,10 +48,10 @@ const NavBar:React.FC = () => {
         {isOpen? <FaX  size={24} onClick={()=>toggleNav()}/>:<FaBars size={24} onClick={()=>toggleNav()} />}</div>
         {isOpen&&(
          <motion.nav
-         initial={{y:-200}}
+         initial={{y:200}}
          whileInView={{y:0}}
          
-         transition={{duration:'0.7'}}
+         transition={{duration:'0.4'}}
          className='md:hidden flex  justify-center items-start   flex-1 bg-red-500  '
          >
           
