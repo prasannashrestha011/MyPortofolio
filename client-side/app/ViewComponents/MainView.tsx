bf5518789cdaf5db97b2d1c785ac8f70a3cea49d
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-10-12 21:38:43 +0545

    changes made on header positioning in phone version

--- client-side/app/ViewComponents/MainView.tsx
+++ client-side/app/ViewComponents/MainView.tsx
@@ -15,10 +15,10 @@ const MainView = () => {
        mx-auto md:w-10/12 md:mt-10 lg:mt-10  '
       
       >
-      <span className='  flex flex-col items-start  justify-between h-60 mt-10 '>
+      <span className='  flex flex-col items-start  justify-evenly h-60 mt-10 ml-4 '>
         <p className='font-semibold md:text-4xl text-2xl '>Prasanna Shrestha</p>
       
-        <p className='md:text-2xl text-xl '>fullstack developer, build Web & Desktop apps</p>
+        <p className='md:text-2xl text-xl w-60 md:w-auto '>fullstack developer, build Web & Desktop apps</p>
         </span>
         <NavBar/>
       </div>
