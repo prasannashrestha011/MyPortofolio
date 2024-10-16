acb528a20c91191265a261bac5c87429f27179ec
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-10-16 20:37:33 +0545

    images size resized

--- client-side/client-side/Types/ThemeStruct.tsx
+++
@@ -1,7 +0,0 @@
-interface ThemeStateStruct{
-    bodyTheme:string,
-    setBodyTheme:(body:string)=>void,
-    foreground:string,
-    setForeground:(foreground:string)=>void,
-}
-export type {ThemeStateStruct}
