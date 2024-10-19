ac55f2bd068e069b491cb569690388275bb35074
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-10-19 22:28:54 +0545

    #3 error

--- client-side/app/api/contact/route.ts
+++ client-side/app/api/contact/route.ts
@@ -7,13 +7,14 @@ export async function POST(req:NextRequest){
         const {username,email,message}= await req.json();
         console.log(username+" -> "+message)
         const response=await SendMessage(username,email,message);
+        
         return NextResponse.json({message:response},{status:200})
     }catch(err){
         const errorMessage = (err as Error).message || 'Unknown error';
 
         console.error(err); // Log the full error for debugging
 
-        // Return a structured error response with detailed information
+      
         return NextResponse.json(
             { error: 'Failed to send message', details: errorMessage },
             { status: 500 }
