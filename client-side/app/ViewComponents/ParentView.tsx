0e600814516d623dcc585f54086a479532ae6eb1
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-12-03 20:49:42 +0545

    added cursor image as default

--- client-side/app/ViewComponents/ParentView.tsx
+++ client-side/app/ViewComponents/ParentView.tsx
@@ -1,32 +1,12 @@
 "use client"
-import React, { ReactNode, useEffect, useState } from 'react'
+import React, { ReactNode  } from 'react'
 import { BackgroudGlow } from '../Components/BackgroudGlow'
 
-interface Prop{
-  posX:number,
-  posY:number,
-}
+
 const ParentView:React.FC<{children:ReactNode}> = ({children}) => {
 
-  const [mousePosition,setMousePosition]=useState<Prop>({
-    posX:0,
-    posY:0
-  })
-  const onMouseMove=(e:MouseEvent)=>{
-   
-      setMousePosition({
-        posX:e.clientX,
-        posY:e.clientY
-      })
-    
-  }
-  useEffect(()=>{
-   window.addEventListener('mousemove',onMouseMove)
-   
-   return()=>{
-    window.removeEventListener('mousemove',onMouseMove)
-   }
-  },[])
+
+ 
   return (
     <div className='relative'>
          <BackgroudGlow/>
