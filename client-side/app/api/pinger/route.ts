a0a7a347c9d44e3a45df4a61537e5047bafdb1ee
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-02-24 17:09:21 +0545

    changes

--- client-side/app/api/pinger/route.ts
+++ client-side/app/api/pinger/route.ts
@@ -1,22 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import OpenProcessesDataManagerSingleton from "@/lib/OpenProcess";
 
-export async function GET() {
-  const processList = OpenProcessesDataManagerSingleton.getInstance();
-  return NextResponse.json({ processes: processList.getData() ?? [] });
+export async function OPTIONS() {
+  const response = new NextResponse(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",  // Allow all domains (or specify one)
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+  return response;
 }
+
 export async function POST(req: NextRequest) {
   const token = process.env.Token;
   const processList= OpenProcessesDataManagerSingleton.getInstance()
   const { processes, access_token } = await req.json();
   console.log(access_token);
-  if (token != access_token) {
-    console.log("Unauthorized access");
-    return NextResponse.json(
-      { message: "Unauthorized access" },
-      { status: 401 },
-    );
-  }
+  
   
   processList.setData(processes)
   console.log("Running processes->", processList);
